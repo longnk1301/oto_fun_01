bf4c0dbab6c17111eb7c89a7f8a1685ac735147f
longnk1301 <khanhlong1315@gmail.com>
2018-05-02 16:11:21 +0700

    fix_floder_path_and_create_welcome_blade_view
    create_dashboard
    
    create_dashboard
    
    fix_floder_path_and_create_welcome_balde_view
    
    delete_old_folder

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    function index () {
+    	return view('welcome');
+    }
+
+    function login () {
+    	return view('user.login');
+    }
+
+    function register () {
+    	return view('user.register');
+    }
+
+}
