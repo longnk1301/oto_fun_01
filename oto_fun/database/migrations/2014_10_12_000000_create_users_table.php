619224115126fbc8bed3042aac92cf229e8c72f7
longnk1301 <khanhlong1315@gmail.com>
2018-05-02 13:41:48 +0700

    fix users table

--- oto_fun/database/migrations/2014_10_12_000000_create_users_table.php
+++ oto_fun/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('user_ name');
-            $table->string('user_email')->unique();
-            $table->string('user_password');
-            $table->integer('user_phone');
-            $table->string('user_add');
-            $table->integer('roles');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
